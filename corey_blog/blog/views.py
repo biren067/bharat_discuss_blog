984f698195ee2f86bf7534982161ce41aeeb32af
Birendra kumar <birendrabitian@gmail.com>
2023-10-12 18:18:06 +0530

    bharat discuss blog

---
+++ corey_blog/blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, HttpResponse
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 23, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 27, 2018'
+    }
+]
+
+
+def get_post(request):
+    # return HttpResponse("<h1>Welcome to new Blog</h1>")
+    context = {'posts': posts, 'title': 'Django Blog'}
+    return render(request, template_name='blog/home.html', context=context)
+
+
+def about(request):
+    # return HttpResponse("<h1> It is our About page</h1>")
+    return render(request, template_name='blog/about.html', context={'name': 'birendra', 'title': "About"})
